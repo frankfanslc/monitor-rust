da4d50585a5d65c58bb087157d8f110482cc921a
Wei Mao <weimao1@gmail.com>
2017-01-31 17:42:19 -0800

    do not refresh window text for universal app

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ fn main() {
     thread::sleep(time::Duration::from_secs(3));
 
     let mut window_handle = win32helper::get_foreground_window();
+    let window_text = win32helper::get_window_text(window_handle);
     let mut process_id = win32helper::get_window_process_id(window_handle);
     let mut process_handle = win32helper::open_process(process_id);
 
@@ -24,9 +25,13 @@ fn main() {
         win32helper::close_handle(process_handle);
         win32helper::get_universal_app(&mut window_handle, &mut process_id);
         process_handle = win32helper::open_process(process_id);
+
+        // Note: I'm not refreshing get_window_text() for universal app here, because if we do so,
+        // the result seems always to be a static string like "Microsoft Edge", which is not useful.
+        // Besides, previous result from the parent app already reads as "CreateFileW - Google Search
+        // and 2 more pages ‎- Microsoft Edge", and I'd rather keep that.
     }
 
-    let window_text = win32helper::get_window_text(window_handle);
     let command_line = win32helper::get_process_command_line(process_handle);
 
     println!("hwnd  : {:?}", window_handle);
