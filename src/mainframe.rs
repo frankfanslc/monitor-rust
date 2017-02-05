d814dd914d8a48e139d41adfbde8c2bf2f203298
Wei Mao <weimao1@gmail.com>
2017-02-05 08:31:54 -0800

    simplify wnd_proc

--- src/mainframe.rs
+++ src/mainframe.rs
@@ -9,35 +9,7 @@ type SimpleTimerCallback = fn();
 
 pub fn setup_periodic_callback(period_in_second: u32, callback: SimpleTimerCallback) {
 
-    fn wnd_proc(hwnd: windef::HWND, msg: minwindef::UINT, wparam: minwindef::WPARAM, lparam: minwindef::LPARAM) -> minwindef::LRESULT {
-
-        win32helper::PeriodicTimer::raw_wnd_proc(hwnd, msg, wparam, lparam);
-
-        match msg {
-            winuser::WM_DESTROY => {
-                win32helper::post_quit_message(0);
-                0
-            }
-            _ => win32helper::def_window_proc(hwnd, msg, wparam, lparam),
-        }
-    }
-
     if win32helper::is_app_already_runniing("Local\\{AB2F0A5E-FAA2-4664-B3C2-25D3984F0A20}") {
-        return;
-    }
-
-    let instance_handle = win32helper::get_current_instance();
-    let class_name = "{8677407E-01E9-4D3E-8BF5-F9082CE08AEB}";
-    let window_name = "Monitor";
-    let wnd_extra: c_int = win32helper::POINTER_SIZE as c_int; // store pointer to win32helper::PeriodicTimer
-
-    let hwnd = win32helper::create_window(class_name,
-                                          window_name,
-                                          wnd_proc,
-                                          winuser::WS_OVERLAPPEDWINDOW | winuser::WS_VISIBLE,
-                                          instance_handle,
-                                          wnd_extra);
-    if hwnd.is_null() {
         return;
     }
 
@@ -48,7 +20,8 @@ pub fn setup_periodic_callback(period_in_second: u32, callback: SimpleTimerCallb
                                                     timer_routine,
                                                     callback as win32helper::TimerContext);
 
-    timer.attach_to_window(hwnd);
+    let hwnd = timer.create_window();
+    timer.register_notification(hwnd);
     timer.start_wait();
 
     win32helper::message_loop();
@@ -60,15 +33,30 @@ fn timer_routine(context: win32helper::TimerContext) {
 }
 
 impl win32helper::PeriodicTimer {
-    pub fn attach_to_window(&mut self, hwnd: windef::HWND) {
+
+    pub fn create_window(&mut self) -> windef::HWND {
+        let instance_handle = win32helper::get_current_instance();
+        let class_name = "{8677407E-01E9-4D3E-8BF5-F9082CE08AEB}";
+        let window_name = "Monitor";
+        let wnd_extra: c_int = win32helper::POINTER_SIZE as c_int; // reserve a space for self pointer
+
+        let hwnd = win32helper::create_window(class_name,
+                                              window_name,
+                                              win32helper::PeriodicTimer::raw_wnd_proc,
+                                              winuser::WS_OVERLAPPEDWINDOW | winuser::WS_VISIBLE,
+                                              instance_handle,
+                                              wnd_extra);
+
         if !win32helper::set_window_long_ptr(hwnd,
                                              0,
                                              self as *mut win32helper::PeriodicTimer as basetsd::LONG_PTR) {
             println!("set_window_long_ptr failed with {:?}",
                      win32helper::get_last_error());
-            return;
         }
+        hwnd
+    }
 
+    pub fn register_notification(&mut self, hwnd: windef::HWND) {
         let power_settings = [win32helper::GUID_SESSION_USER_PRESENCE, win32helper::GUID_SESSION_DISPLAY_STATUS];
         for setting in power_settings.iter() {
             if win32helper::register_power_setting_notification(hwnd as winnt::HANDLE,
@@ -87,11 +75,19 @@ impl win32helper::PeriodicTimer {
         }
     }
 
-    pub fn raw_wnd_proc(hwnd: windef::HWND, msg: minwindef::UINT, wparam: minwindef::WPARAM, lparam: minwindef::LPARAM) {
+    pub fn raw_wnd_proc(hwnd: windef::HWND, msg: minwindef::UINT, wparam: minwindef::WPARAM, lparam: minwindef::LPARAM) -> minwindef::LRESULT {
         let raw_ptr = win32helper::get_window_long_ptr(hwnd, 0);
         if raw_ptr != 0 {
             let timer = unsafe { &mut *(raw_ptr as *mut win32helper::PeriodicTimer) };
             timer.wnd_proc(hwnd, msg, wparam, lparam);
+        }
+
+        match msg {
+            winuser::WM_DESTROY => {
+                win32helper::post_quit_message(0);
+                0
+            }
+            _ => win32helper::def_window_proc(hwnd, msg, wparam, lparam),
         }
     }
 
